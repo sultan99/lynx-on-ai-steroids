48bd4b92415084aac513b798250e8bc4a140a9d5
sultan99 <sultan99@mail.ru>
2026-03-03 02:38:44 +0500

    🧹chore: remove unused bookmarks screen and route

--- src/app/routes/bookmarks.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { BookmarksScreen } from '@/screens/bookmarks'
-
-export const Route = createFileRoute('/bookmarks')({
-  component: BookmarksScreen,
-})
