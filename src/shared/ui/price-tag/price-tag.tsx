213d2e7b76b6335c3180e8b3ee1da6d3304ff877
sultan99 <sultan99@mail.ru>
2026-03-10 11:25:52 +0500

    🧹chore(ui): remove unused PriceTag component

--- src/shared/ui/price-tag/price-tag.tsx
+++
@@ -1,9 +0,0 @@
-import * as css from './price-tag.module.scss'
-
-type PriceTagProps = {
-  price: number
-}
-
-export const PriceTag = ({ price }: PriceTagProps) => (
-  <text className={css.root}>${price.toFixed(2)}</text>
-)
